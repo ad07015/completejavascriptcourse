fb3f14c37b1bd1e183570eb633d731a0a8e5db50
andrejs.dasko <andrejs.dasko@accenture.com>
2019-04-09 20:07:39 +0300

    Coding challenge 1 complete

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -56,7 +56,7 @@ console.log(firstName + ' ' + lastName);
 /**
 * Basic operators
 */
-
+/*
 var currentYear, yearJohn, yearMark, ageJohn, ageMark
 currentYear = 2018;
 ageJohn = 28;
@@ -81,6 +81,69 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
 console.log(typeof x);
+*/
+
+// Operator precedense
+/*
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Mulitple opereators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+
+// Grouping
+var averageAgeJohnMark = (ageJohn + ageMark) / 2;
+console.log(averageAgeJohnMark);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6;
+console.log(x, y);
+
+// More operators
+
+x = x * 2;
+console.log("x = x * 2: " + x);
+x *= 2;
+console.log("x *= 2: " + x);
+x = x + 1;
+console.log(x);
+x += 1;
+console.log(x);
+x++;
+console.log(x);
+*/
+
+// Coding challenge 1
+
+var massMark, massJohn, heightMark, heightJohn, bmiMark, bmiJohn;
+massMark = 78;
+heightMark = 1.69;
+
+massJohn = 92;
+heightJohn = 1.95;
+
+bmiMark = massMark / (heightMark * heightMark);
+bmiJohn = massJohn / (heightJohn * heightJohn);
+
+var markBmiHigher = bmiMark > bmiJohn;
+
+console.log("Mark's BMI: " + bmiMark);
+console.log("John's BMI: " + bmiJohn);
+console.log(bmiMark, bmiJohn);
+console.log("Is Mark's BMI higher than John's? " + markBmiHigher);
+
+
+
+
+
+
+
 
 
 
